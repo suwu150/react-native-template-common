0b330f2cbf70c12504c4aaa89886960c5a9fbb27
jkwu <jkwu@amarsoft.com>
2018-04-27 21:17:38 +0800

    feat: modify pages and modify explain

--- src/network/netConfig.js
+++ src/network/netConfig.js
@@ -1,7 +1,7 @@
 
 const nativeConfig = {
-  Host: 'http://192.168.137.1:3003/leasing',
-  BasePath: '/webapi/resf',
+  Host: 'http://192.168.137.1:3003/lease',
+  BasePath: '/example',
   // OuterHost: 'https://wxcs.s1.natapp.cc',
   // BaiduMapHost: 'http://api.map.baidu.com'
 };
